bca8d6f6c87295a2f06f87b83170db1a2c0cd710
Georgy <v-georgypuzakov@github.com>
2026-03-20 12:24:09 +0100

    add fix for brew

--- images/macos/scripts/build/install-llvm.sh
+++ images/macos/scripts/build/install-llvm.sh
@@ -10,4 +10,7 @@ llvmVersion=$(get_toolset_value '.llvm.version')
 
 brew_smart_install "llvm@${llvmVersion}"
 
+echo "Ensuring Apple clang remains the default compiler"
+brew unlink "llvm@${llvmVersion}"
+
 invoke_tests "LLVM"
